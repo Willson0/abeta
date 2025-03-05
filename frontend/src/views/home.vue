17e2bbd37cbd2431d98d301965770b50b83bc5c2
Willson0 <logvinovnikita3@gmail.com>
2025-03-06 03:19:20 +0500

    d

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -117,9 +117,13 @@
                         "Content-Type": "application/json",
                     }
                 }).then((response) => {
+                    alert (response.ok)
                     if (!response.ok) {
                         let el = document.querySelector(".forbiddenPopup");
+                        alert (el.style.display);
                         el.style.display = "flex";
+                        alert (el.style.display);
+
                     }
                     return response.json();
                 }).then((response) => {
