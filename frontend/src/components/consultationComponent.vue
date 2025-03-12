b9fc68722e128006dc4e72eefc6d7a2b88364d78
Willson0 <logvinovnikita3@gmail.com>
2025-03-13 03:50:15 +0500

    d

--- frontend/src/components/consultationComponent.vue
+++ frontend/src/components/consultationComponent.vue
@@ -95,7 +95,7 @@ export default {
             </form>
             <div class="form_policy">Нажимая на кнопку, вы соглашаетесь <a>с политикой конфиденциальности</a></div>
         </div>
-        <div class="feed_consultation" v-else style="margin-top:16px;">
+        <div class="feed_consultation" v-else style="margin-top:16px; margin-left:0;width:100%;box-sizing: border-box">
             <div class="feed_consultation_title" style="margin-top:0;">
                 Вы записаны на консультацию
             </div>
