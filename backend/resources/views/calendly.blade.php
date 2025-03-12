ecacb7a1c1832502cd726cd43b1e2ddd46609d07
Willson0 <logvinovnikita3@gmail.com>
2025-03-13 02:16:39 +0500

    d

--- backend/resources/views/calendly.blade.php
+++ backend/resources/views/calendly.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Success</title>
 
     <style>
@@ -41,7 +42,7 @@
 <body>
 <div class="notify_calendly">
     <div>
-        <img src="{{ asset('successBubble.png') }}" alt="">
+        <img src="{{ url('successBubble.png') }}" alt="">
         <div class="notify_calendly_text">Можете вернуться<br>в Telegram</div>
     </div>
 </div>
