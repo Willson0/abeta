8031a4cad36156d33f49aa169111ad9b68be1641
Willson0 <logvinovnikita3@gmail.com>
2025-04-03 16:24:53 +0500

    d

--- backend/database/migrations/2025_02_20_204943_add_column_bio_to_users_table.php
+++ backend/database/migrations/2025_02_20_204943_add_column_bio_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string("bio")->after("patronymic")->default(" ");
+            $table->string("bio")->nullable()->after("patronymic")->default(" ");
         });
     }
 
