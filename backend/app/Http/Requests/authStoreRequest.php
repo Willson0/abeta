edccc211a9c03ed32f7db55aa6e53dd5db507785
Willson0 <logvinovnikita3@gmail.com>
2025-04-04 16:51:48 +0500

    d

--- backend/app/Http/Requests/authStoreRequest.php
+++ backend/app/Http/Requests/authStoreRequest.php
@@ -25,7 +25,7 @@ class authStoreRequest extends FormRequest
             "fullname" => 'regex:/^[А-ЯЁ][а-яё]+(?: [А-ЯЁ][а-яё]+){0,2}$/u',
             "bio" => "string|nullable",
             "phone" => [
-                "nullable|regex:/^\+?[1-9]\d{1,14}$/"
+                "sometimes|nullable|regex:/^\+?[1-9]\d{1,14}$/"
             ],
             "notifications" => "boolean",
             "expert_mailing" => "boolean",
