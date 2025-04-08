551249bad142979a7cbc7e7a39dfac914de4cdd5
Willson0 <logvinovnikita3@gmail.com>
2025-04-09 01:13:16 +0500

    d

--- frontend/src/views/analyticsView.vue
+++ frontend/src/views/analyticsView.vue
@@ -88,7 +88,7 @@ export default {
         async sendData() {
             for (let field in this.fields) {
                 if (!this.fields[field]) return alert("Заполните все поля!");
-                if (field.toLocaleLowerCase() === "телефон" && !/(?:\D*\d){10,15}/.test(this.fields[field])) return alert ("Неправильный формат номера телефона");
+                if (field.toLocaleLowerCase() === "телефон" && (!/(?:\D*\d){10,15}/.test(this.fields[field]) || this.fields[field].length > 15)) return alert ("Неправильный формат номера телефона");
                 if ((field.toLocaleLowerCase() === "имя" || field.toLocaleLowerCase() === "фио") && !/^[А-ЯЁ][а-яё]+(?: [А-ЯЁ][а-яё]+)?(?: [А-ЯЁ][а-яё]+)?$/.test(this.fields[field])) return alert ("Неправильный формат ФИО");
             }
 
--- frontend/src/views/webinarView.vue
+++ frontend/src/views/webinarView.vue
@@ -100,7 +100,7 @@ export default {
             document.querySelector(".webinar_registration_error").classList.remove("active");
             for (let field in this.fields) {
                 if (!this.fields[field]) return document.querySelector(".webinar_registration_error").classList.add("active");
-                if (field.toLocaleLowerCase() === "телефон" && !/(?:\D*\d){10,15}/.test(this.fields[field])) return alert ("Неправильный формат номера телефона");
+                if (field.toLocaleLowerCase() === "телефон" && (!/(?:\D*\d){10,15}/.test(this.fields[field]) || this.fields[field].length > 15)) return alert ("Неправильный формат номера телефона");
                 if ((field.toLocaleLowerCase() === "имя" || field.toLocaleLowerCase() === "фио") && !/^[А-ЯЁ][а-яё]+(?: [А-ЯЁ][а-яё]+)?(?: [А-ЯЁ][а-яё]+)?$/.test(this.fields[field])) return alert ("Неправильный формат ФИО");
             }
 
