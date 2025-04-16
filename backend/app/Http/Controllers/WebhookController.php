f9068532dabf6493fe9656d4a274499dbf862ece
Willson0 <logvinovnikita3@gmail.com>
2025-04-16 21:29:02 +0300

    d

--- backend/app/Http/Controllers/WebhookController.php
+++ backend/app/Http/Controllers/WebhookController.php
@@ -963,25 +963,25 @@ class WebhookController extends Controller
 
             $urlReaction = "https://api.telegram.org/bot$token/setMessageReaction";
             if ($user->step === "support") {
+                if ($message["text"] == "/close") {
+                    $response = Http::post($sendurl, [
+                        'chat_id' => $user->telegram_id,
+                        'text' => "Вы уверены, что хотите закрыть этот чат?",
+                        "reply_markup" => [
+                            "inline_keyboard" => [
+                                [["text" => "✅ Да", "callback_data" => "admin_support_close"]],
+                            ]
+                        ]
+                    ]);
+                    return response("", 200);
+                }
+
                 $support = Support::where("user_id", $user->id)->whereColumn("user_id", "!=", "admin_id")->whereNotNull("admin_id")->first();
                 if (!$support) {
                     $admin = Admin::where("telegram_id", $user->telegram_id)->first();
                     if (!$admin) {
                         $user->step = "";
                         $user->save();
-                        return response("", 200);
-                    }
-
-                    if ($message["text"] == "/close") {
-                        $response = Http::post($sendurl, [
-                            'chat_id' => $user->telegram_id,
-                            'text' => "Вы уверены, что хотите закрыть этот чат?",
-                            "reply_markup" => [
-                                "inline_keyboard" => [
-                                    [["text" => "✅ Да", "callback_data" => "admin_support_close"]],
-                                ]
-                            ]
-                        ]);
                         return response("", 200);
                     }
 
